f862954c6d0a7218194d1788631b0d4ab057263c
Nguyethue1905 <nguyethue1905@gmail.com>
2023-11-14 22:00:48 +0700

    ok

---
+++ duan1/user/model/include/sidebar.php
@@ -0,0 +1,73 @@
+<div class="col-lg-3">
+    <aside class="sidebar static">
+        <div class="widget">
+            <h4 class="widget-title">Lối tắt</h4>
+            <ul class="naves">
+                <li>
+                    <i class="ti-clipboard"></i>
+                    <a href="../user/index.php?act=home" title="">Bảng tin</a>
+                </li>
+                <li>
+                    <i class="ti-files"></i>
+                    <a href="../user/index.php?act=myacount" title="">Trang cá nhân</a>
+                </li>
+                <li>
+                    <i class="ti-user"></i>
+                    <a href="timeline-friends.html" title="">Bạn bè</a>
+                </li>
+                <li>
+                    <i class="ti-comments-smiley"></i>
+                    <a href="messages.html" title="">Tin nhắn</a>
+                </li>
+                <li>
+                    <i class="ti-bell"></i>
+                    <a href="notifications.html" title="">Thông báo</a>
+                </li>
+                <li>
+                    <i class="ti-power-off"></i>
+                    <a href="landing.html" title="">Đăng xuất</a>
+                </li>
+            </ul>
+        </div><!-- Shortcuts -->
+        <div class="widget stick-widget">
+            <h4 class="widget-title">Gợi ý kết bạn</h4>
+            <ul class="followers">
+                <li>
+                    <figure><img src="images/resources/friend-avatar2.jpg" alt=""></figure>
+                    <div class="friend-meta">
+                        <h4><a href="time-line.html" title="">Kelly Bill</a></h4>
+                        <a href="#" title="" class="underline">Add Friend</a>
+                    </div>
+                </li>
+                <li>
+                    <figure><img src="images/resources/friend-avatar4.jpg" alt=""></figure>
+                    <div class="friend-meta">
+                        <h4><a href="time-line.html" title="">Issabel</a></h4>
+                        <a href="#" title="" class="underline">Add Friend</a>
+                    </div>
+                </li>
+                <li>
+                    <figure><img src="images/resources/friend-avatar6.jpg" alt=""></figure>
+                    <div class="friend-meta">
+                        <h4><a href="time-line.html" title="">Andrew</a></h4>
+                        <a href="#" title="" class="underline">Add Friend</a>
+                    </div>
+                </li>
+                <li>
+                    <figure><img src="images/resources/friend-avatar8.jpg" alt=""></figure>
+                    <div class="friend-meta">
+                        <h4><a href="time-line.html" title="">Sophia</a></h4>
+                        <a href="#" title="" class="underline">Add Friend</a>
+                    </div>
+                </li>
+                <li>
+                    <figure><img src="images/resources/friend-avatar3.jpg" alt=""></figure>
+                    <div class="friend-meta">
+					<h4><a href="time-line.html" title="">Allen</a></h4>
+                        <a href="#" title="" class="underline">Add Friend</a>
+                    </div>
+                </li>
+            </ul>
+        </div><!-- who's following -->
+    </aside>
+</div><!-- sidebar -->
